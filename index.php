a605ed6f85c554502b24ca86889b216cad3366f0
livv <livv>
2018-02-12 12:14:12 +0100

    no message

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MadLibs</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header id="container">
+        <img src="logo.png">
+        <nav id="menu">
+            <ul>
+                <li><a href="index.php">Er heerst paniek...</a></li>
+                <li><a href="onkunde.php">Onkunde</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+        <h3>Er heerst paniek...</h3>
+            <form action="#" method="POST">
+                <p>Welk dier zou je nooit als huisdier willen hebben?</p>
+                    <input type="text" name="ant1">
+                <p>Wie is de belangrijkste persoon in je leven?</p>
+                    <input type="text" name="ant2">
+                <p>In welk land zou je graag willen wonen?</p>
+                    <input type="text" name="ant3">
+                <p>Wat doe je als je je verveelt?</p>
+                    <input type="text" name="ant4">
+                <p>Met welk speelgoed speelde je als kind het meest?</p>
+                    <input type="text" name="ant5">
+                <p>Bij welke docent spijbel je het liefst?</p>
+                    <input type="text" name="ant6">
+                <p>Als je €100.000,- had, wat zou je dan kopen?</p>
+                    <input type="text" name="ant7">
+                <p>Wat is je favoriete bezigheid?</p>
+                    <input type="text" name="ant8">
+
+                    <input type="submit" name="submit">
+            </form>
+    </main>
+
+</body>
+<footer>.</footer>
+</html>
---
+++ workA.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+     <header id="container">
+        <img src="logo.png">
+        <nav id="menu">
+            <ul>
+                <li><a href="index.php">Er heerst paniek...</a></li>
+                <li><a href="onkunde.php">Onkunde</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+    
+    </main>
+    
+</body>
+</html>
